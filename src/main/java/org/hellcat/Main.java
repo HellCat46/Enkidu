5de05846c6e4d7e99bbfcd0cfe7775272ebefc2f
HellCat46 <hellcat2388@gmail.com>
2024-01-06 18:07:29 +0530

    Better Error Handling for Inserting Row Section

--- src/main/java/org/hellcat/Main.java
+++ src/main/java/org/hellcat/Main.java
@@ -1,11 +1,11 @@
 package org.hellcat;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -13,7 +13,8 @@ public class Main {
     static Connection dbCon;
     static FileInputStream csvFile;
     static boolean EOF = false;
-    public static void main(String[] args)  {
+
+    public static void main(String[] args) {
 
         // Get Arguments Provided by user
         final ToolArguments Arguments = ParseArguments(args);
@@ -29,22 +30,22 @@ public class Main {
 
         // Open a Read Stream the CSV File
         int LineNumber = 1;
-        try{
+        try {
             csvFile = new FileInputStream(Arguments.CSVPath);
-        }catch (IOException ex){
-            System.out.println("Errors while trying to Read the CSV File\n"+ex);
+        } catch (IOException ex) {
+            System.out.println("Errors while trying to Read the CSV File\n" + ex);
             return;
         }
 
 
         // Parses Out all the Column Name in the CSV File
         Optional<ArrayList<String>> readRes = ReadColumnNames(Arguments.delimiter);
-        if(readRes.isEmpty()){
-            System.out.println("Unable to Read File At Line"+ LineNumber);
+        if (readRes.isEmpty()) {
+            System.out.println("Unable to Read File At Line" + LineNumber);
             return;
         }
         ArrayList<String> columns = readRes.get();
-        if(columns.isEmpty()){
+        if (columns.isEmpty()) {
             System.out.println("Error: Unable to parse any columns from CSV. Make sure you have selected the correct Delimiter");
             return;
         }
@@ -52,21 +53,21 @@ public class Main {
 
         // Parses Out First Row Elements to Determine Column's Datatype and to obviously get first row
         readRes = ReadLine(Arguments.delimiter);
-        if(readRes.isEmpty()){
+        if (readRes.isEmpty()) {
             System.out.println("Unable to Read File Line");
             return;
         }
         ArrayList<String> RowElements = readRes.get();
-        if(RowElements.size() != columns.size()){
-            System.out.println("Line "+LineNumber+":Either Too Few or Too many Attributes.");
+        if (RowElements.size() != columns.size()) {
+            System.out.println("Line " + LineNumber + ":Either Too Few or Too many Attributes.");
             return;
         }
         LineNumber++;
 
         // Determines Column's Datatype
         ArrayList<String> ColumnTypes = new ArrayList<>();
-        for(String ele : RowElements){
-            if(Parsable.toInt(ele))
+        for (String ele : RowElements) {
+            if (Parsable.toInt(ele))
                 ColumnTypes.add("int");
             else if (Parsable.toFloat(ele))
                 ColumnTypes.add("float");
@@ -90,29 +91,32 @@ public class Main {
             }
         }
 
-        try {
-            Optional<ArrayList<String >> res;
-            while (!EOF){
-                String InsertDataQuery = BuildDbInsertQuery(name, columns, RowElements, ColumnTypes);
-                statement.executeUpdate(InsertDataQuery);
+        Optional<ArrayList<String>> res;
+        while (!EOF) {
+            String InsertDataQuery = BuildDbInsertQuery(name, columns, RowElements, ColumnTypes);
 
-                res = ReadLine(Arguments.delimiter);
-                if(res.isEmpty()) System.out.println("Unable to Read Line No "+(LineNumber+1)+". Skipping...");
-                else{
-                    RowElements = res.get();
-                    System.out.println("Successfully Read Line No "+(++LineNumber));
-                }
+            try {
+                statement.executeUpdate(InsertDataQuery);
+            } catch (SQLException ex) {
+                System.out.println("Failed to Insert Lineno "+LineNumber+" Into the Table. Skipping..." + ex);
             }
 
-        }catch (SQLException ex){
-            System.out.println("Unable to Insert Data Into the Table"+ex);
+            res = ReadLine(Arguments.delimiter);
+            if (res.isEmpty()) System.out.println("Unable to Read Line No " + (LineNumber + 1) + ". Skipping...");
+            else {
+                RowElements = res.get();
+                System.out.println("Successfully Read Line No " + (++LineNumber));
+            }
         }
+        System.out.println("LETS FUCKING GOOOOOOOOOOOOOOOOOO");
+
+
     }
 
-    public static String BuildDbCreateQuery(String tableName, ArrayList<String> columns, ArrayList<String> ColumnTypes){
+    public static String BuildDbCreateQuery(String tableName, ArrayList<String> columns, ArrayList<String> ColumnTypes) {
 
-        StringBuilder query = new StringBuilder("CREATE TABLE "+tableName+"(\""+columns.getFirst()+"\" "+ColumnTypes.getFirst());
-        for(int i =1; i< ColumnTypes.size(); i++){
+        StringBuilder query = new StringBuilder("CREATE TABLE " + tableName + "(\"" + columns.getFirst() + "\" " + ColumnTypes.getFirst());
+        for (int i = 1; i < ColumnTypes.size(); i++) {
             query.append(", \"")
                     .append(columns.get(i))
                     .append("\" ")
@@ -124,88 +128,95 @@ public class Main {
     }
 
     public static String BuildDbInsertQuery(String tableName, ArrayList<String> Columns, ArrayList<String> Row, ArrayList<String> ColumnTypes) {
-        StringBuilder queryTable = new StringBuilder("INSERT INTO "+tableName+"(");
+        StringBuilder queryTable = new StringBuilder("INSERT INTO " + tableName + "(");
         StringBuilder queryValues = new StringBuilder("VALUES(");
-        for(int i =0; i< Row.size(); i++){
-            if(Row.get(i).isEmpty()) continue;
+        for (int i = 0; i < Row.size(); i++) {
+            if (Row.get(i).isEmpty()) continue;
 
             queryTable.append("\"").append(Columns.get(i)).append("\"").append(",");
-            if(ColumnTypes.get(i).equals("text") ){
+            if (ColumnTypes.get(i).equals("text")) {
                 queryValues.append("'").append(Row.get(i)).append("',");
-            }else {
+            } else {
                 queryValues.append(Row.get(i)).append(",");
             }
         }
-        queryTable.deleteCharAt(queryTable.length()-1);
+        queryTable.deleteCharAt(queryTable.length() - 1);
         queryTable.append(") ");
-        queryValues.deleteCharAt(queryValues.length()-1);
+        queryValues.deleteCharAt(queryValues.length() - 1);
         queryValues.append(");");
         return queryTable.toString() + queryValues;
     }
-    private static Optional<ArrayList<String>> ReadColumnNames(ToolArguments.Delimiter delimiter){
+
+    private static Optional<ArrayList<String>> ReadColumnNames(ToolArguments.Delimiter delimiter) {
         try {
             ArrayList<String> list = new ArrayList<>();
             StringBuilder colname = new StringBuilder();
 
             int character;
             boolean openQuote = false;
-            while ((character = csvFile.read()) != 10){
-                if(character == -1){
+            while ((character = csvFile.read()) != 10) {
+                if (character == 34) {
+                    openQuote = !openQuote;
+                    continue;
+                }
+                if (character == delimiter.val && !openQuote) {
+                    if (colname.isEmpty()) colname.append("Col").append(list.size() - 1);
+
+                    list.add(colname.toString());
+                    colname = new StringBuilder();
+                    continue;
+                }
+
+                if (character == 39) colname.append("'");
+
+                colname.append((char) character);
+            }
+
+            if (!colname.isEmpty()) {
+                list.add(colname.toString());
+            }
+            return Optional.of(list);
+        } catch (Exception ex) {
+            return Optional.empty();
+        }
+    }
+
+    private static Optional<ArrayList<String>> ReadLine(ToolArguments.Delimiter delimiter) {
+        try {
+            ArrayList<String> list = new ArrayList<>();
+            StringBuilder colname = new StringBuilder();
+
+            int character;
+            boolean openQuote = false;
+            while ((character = csvFile.read()) != 10 ) {
+                if (character == -1) {
                     EOF = true;
                     return Optional.empty();
                 }
 
-                if(character == 34){
+                if (character == 34) {
                     openQuote = !openQuote;
                     continue;
                 }
-                if(character == delimiter.val && !openQuote){
-                    if(colname.isEmpty()) colname.append("Col").append(list.size()-1);
-
+                if (character == delimiter.val && !openQuote) {
                     list.add(colname.toString());
                     colname = new StringBuilder();
                     continue;
                 }
+
+                if (character == 39) colname.append("'");
+
                 colname.append((char) character);
             }
-
-            if(!colname.isEmpty()){
+            if (!colname.isEmpty()) {
                 list.add(colname.toString());
             }
             return Optional.of(list);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return Optional.empty();
         }
     }
 
-    private static Optional<ArrayList<String>> ReadLine(ToolArguments.Delimiter delimiter){
-        try {
-            ArrayList<String> list = new ArrayList<>();
-            StringBuilder colname = new StringBuilder();
-
-            int character;
-            boolean openQuote = false;
-            while ((character = csvFile.read()) != 10){
-
-                if(character == 34){
-                    openQuote = !openQuote;
-                    continue;
-                }
-                if(character == delimiter.val && !openQuote){
-                    list.add(colname.toString());
-                    colname = new StringBuilder();
-                    continue;
-                }
-                colname.append((char) character);
-            }
-            if(!colname.isEmpty()){
-                list.add(colname.toString());
-            }
-            return Optional.of(list);
-        }catch (Exception ex){
-            return Optional.empty();
-        }
-    }
     private static ToolArguments ParseArguments(String[] args) {
         ToolArguments arguments = new ToolArguments();
         for (String arg : args) {
@@ -216,8 +227,7 @@ public class Main {
                     System.out.println("Error: Provided CSV Path is invalid.\n");
                     System.exit(1);
                 }
-            }
-            else if (arg.contains("--delimiter=")) {
+            } else if (arg.contains("--delimiter=")) {
                 if (arg.contains("C")) arguments.delimiter = ToolArguments.Delimiter.Comma;
                 else if (arg.contains("S")) arguments.delimiter = ToolArguments.Delimiter.Semicolon;
                 else if (arg.contains("P")) arguments.delimiter = ToolArguments.Delimiter.Pipe;
@@ -226,47 +236,47 @@ public class Main {
                     System.out.println("Error: Unknown Delimiter. Valid Delimiters are C,S,P,T.\n");
                     System.exit(1);
                 }
-            }
-            else if (arg.contains("--dbip=")) arguments.dbIP = Arrays.stream(arg.split("--dbip=")).toList().getLast();
-            else if (arg.contains("--dbname=")) arguments.dbName = Arrays.stream(arg.split("--dbname=")).toList().getLast();
-            else if (arg.contains("--dbuser=")) arguments.dbUsername = Arrays.stream(arg.split("--dbuser=")).toList().getLast();
-            else if (arg.contains("--dbpass=")) arguments.dbPassword = Arrays.stream(arg.split("--dbpass=")).toList().getLast();
+            } else if (arg.contains("--dbip=")) arguments.dbIP = Arrays.stream(arg.split("--dbip=")).toList().getLast();
+            else if (arg.contains("--dbname="))
+                arguments.dbName = Arrays.stream(arg.split("--dbname=")).toList().getLast();
+            else if (arg.contains("--dbuser="))
+                arguments.dbUsername = Arrays.stream(arg.split("--dbuser=")).toList().getLast();
+            else if (arg.contains("--dbpass="))
+                arguments.dbPassword = Arrays.stream(arg.split("--dbpass=")).toList().getLast();
             else if (arg.contains("--dbtype=")) {
-                 if(arg.contains("mssql")) arguments.dbType = ToolArguments.DatabaseType.MSSQL;
-                 else if(arg.contains("mysql")) arguments.dbType = ToolArguments.DatabaseType.MYSQL;
-                 else if(arg.contains("postgres")) arguments.dbType = ToolArguments.DatabaseType.POSTGRESQL;
-                 else {
-                     System.out.println("Error: Unknown Database Type. Valid Types are mssql, mysql and postgres");
-                     System.exit(1);
-                 }
-            }
-            else if (arg.contains("--help")) {
+                if (arg.contains("mssql")) arguments.dbType = ToolArguments.DatabaseType.MSSQL;
+                else if (arg.contains("mysql")) arguments.dbType = ToolArguments.DatabaseType.MYSQL;
+                else if (arg.contains("postgres")) arguments.dbType = ToolArguments.DatabaseType.POSTGRESQL;
+                else {
+                    System.out.println("Error: Unknown Database Type. Valid Types are mssql, mysql and postgres");
+                    System.exit(1);
+                }
+            } else if (arg.contains("--help")) {
                 printDefaultOutput();
                 System.exit(1);
-            }
-            else {
+            } else {
                 System.out.println("Unknown Argument: " + arg + "\n");
                 printDefaultOutput();
                 System.exit(1);
             }
         }
 
-        if(arguments.CSVPath == null || arguments.delimiter == null || arguments.dbIP == null || arguments.dbName == null || arguments.dbUsername == null || arguments.dbPassword == null || arguments.dbType == null) {
+        if (arguments.CSVPath == null || arguments.delimiter == null || arguments.dbIP == null || arguments.dbName == null || arguments.dbUsername == null || arguments.dbPassword == null || arguments.dbType == null) {
             System.out.println("Error: Too few Arguments.\n");
             printDefaultOutput();
             System.exit(1);
         }
 
 
-        try{
+        try {
             InitializeDatabase(arguments);
-            if(dbCon.isValid(30))
+            if (dbCon.isValid(30))
                 System.out.println("Success");
-            else{
+            else {
                 System.out.println("Failed");
             }
-        }catch (SQLException ex){
-            System.out.println("Unable to connect to the Database.\n"+ex);
+        } catch (SQLException ex) {
+            System.out.println("Unable to connect to the Database.\n" + ex);
             System.exit(1);
         }
 
@@ -275,7 +285,7 @@ public class Main {
 
     }
 
-    private static  void InitializeDatabase(ToolArguments arguments){
+    private static void InitializeDatabase(ToolArguments arguments) {
         try {
             if (arguments.dbType == ToolArguments.DatabaseType.MSSQL) {
                 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -286,30 +296,32 @@ public class Main {
             System.exit(1);
         }
     }
-    private static void printDefaultOutput(){
-        System.out.println("""
-                    \033[1mUsage: enkidu [options...]
 
-                    Option           Usage                         Description
-                    csvpath          --csvpath=[filepath]          Relative Path to the CSV File
-                    delimiter        --delimiter=[C,S,P,T]         Delimiter's First Character
-                    dbip             --dbip=[ipAddr]               Database's IP Address And Port
-                    dbname           --dbname=[name]               Database's Name
-                    dbuser           --dbuser=[username]           Database User's Name
-                    dbpass           --dbpass=[password]           Database User's Password
-                    dbtype           --dbpas=[type]                Database Type
-                    """);
+    private static void printDefaultOutput() {
+        System.out.println("""
+                \033[1mUsage: enkidu [options...]
+
+                Option           Usage                         Description
+                csvpath          --csvpath=[filepath]          Relative Path to the CSV File
+                delimiter        --delimiter=[C,S,P,T]         Delimiter's First Character
+                dbip             --dbip=[ipAddr]               Database's IP Address And Port
+                dbname           --dbname=[name]               Database's Name
+                dbuser           --dbuser=[username]           Database User's Name
+                dbpass           --dbpass=[password]           Database User's Password
+                dbtype           --dbpas=[type]                Database Type
+                """);
     }
 }
 
 class ToolArguments {
     public Delimiter delimiter = null;
     public String CSVPath = null;
-    public String dbIP =null;
-    public String dbName =null;
+    public String dbIP = null;
+    public String dbName = null;
     public String dbUsername = null;
     public String dbPassword = null;
     public DatabaseType dbType = null;
+
     enum Delimiter {
         Comma((byte) 44),
         Semicolon((byte) 59),
@@ -317,44 +329,48 @@ class ToolArguments {
         Tab((byte) 9);
 
         public final byte val;
-        Delimiter(byte val){
+
+        Delimiter(byte val) {
             this.val = val;
         }
     }
+
     enum DatabaseType {
         MYSQL("mysql"),
         MSSQL("sqlserver"),
         POSTGRESQL("postgresql");
 
         public final String value;
-        DatabaseType(String value){
+
+        DatabaseType(String value) {
             this.value = value;
         }
     }
 
-    public String GetDatabaseURL(){
-        if(dbType == DatabaseType.MSSQL){
-            return "jdbc:"+dbType.value+"://"+dbIP+";database="+dbName+";trustServerCertificate=true";
-        }else{
-            return "jdbc:"+dbType.value+"://"+dbIP+"/"+dbName;
+    public String GetDatabaseURL() {
+        if (dbType == DatabaseType.MSSQL) {
+            return "jdbc:" + dbType.value + "://" + dbIP + ";database=" + dbName + ";trustServerCertificate=true";
+        } else {
+            return "jdbc:" + dbType.value + "://" + dbIP + "/" + dbName;
         }
     }
 }
 
 class Parsable {
-    public static boolean toFloat(String input){
+    public static boolean toFloat(String input) {
         try {
             Float.parseFloat(input);
             return true;
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
             return false;
         }
     }
-    public static boolean toInt(String input){
+
+    public static boolean toInt(String input) {
         try {
             Integer.parseInt(input);
             return true;
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
             return false;
         }
     }
